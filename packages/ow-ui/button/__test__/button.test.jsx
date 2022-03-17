71d1b0b68dcadb71ae9dffbdd916d6c8055deedb
maple <fengtangbuding@gmail.com>
2022-03-17 14:31:58 +0800

    feat: temp
    temp commit

--- packages/ow-ui/button/__test__/button.test.jsx
+++ packages/ow-ui/button/__test__/button.test.jsx
@@ -11,6 +11,8 @@ describe('Button', () =>{
 
   test('button with custom className & style', () =>{
     render(<Button className='testClass' style={{background:'black'}}>test</Button>)
+    expect(screen.getByText('test')).toHaveClass('testClass')
+    expect(screen.getByText('test')).toHaveStyle('background: black')
   })
 
   test('button disabled', () =>{
